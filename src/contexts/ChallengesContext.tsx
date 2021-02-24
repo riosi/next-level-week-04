a66e3b8f230bb653eaa44f74bc0aee1d8b1eb9a2
Ingryd Rios <riosingryd@gmail.com>
2021-02-24 16:44:58 -0300

    change of state

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -31,7 +31,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const [activeChallenge, setActiveChallenge] = useState(null);
 
-  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
   function levelUp() {
     setLevel(level + 1);
