1143185ed92bcec4184eddcfe7a60a42d86fb0ad
Ingryd Rios <riosingryd@gmail.com>
2021-02-25 17:41:35 -0300

    :bento: finished leveling structure

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -36,6 +36,7 @@ export function CountdownProvider ({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeOut);
     setIsActive(false);
+    setHasFinished(false);
     setTime(0.1 * 60);
   }
 
